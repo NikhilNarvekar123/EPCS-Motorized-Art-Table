c1e020fcce869b0dd10ea20dc4d2e8d6bbe36e89
Nikhil Narvekar <31781542+NikhilNarvekar123@users.noreply.github.com>
2022-04-14 19:57:24 -0500

    Update temp.ino

--- temp.ino
+++ temp.ino
@@ -1,24 +1,28 @@
 #include <IRremote.h>
 
+// pins
 const int click_pin = 2;
 const int motor_in3 = 4;
 const int motor_in4 = 3;
 const int motor_dir_pin = 10;
 const int motor_step_pin = 9;
 const int enable = 13;
-const int steps_per_rev = 1000;
+const int steps_per_rev = 16000;
 const int receiver_pin = 7;
+
+// temp
+const int relay_pin1 = 5;
+const int relay_pin2 = 6;
 
 // setup receiver
 IRrecv irrecv(receiver_pin);
 decode_results results;
 
 // remote variables
-const unsigned int U_MAX_INT = 4294967295;
-const int UP_VAL = 16750695;
-const int DOWN_VAL = 16726215;
-const int LEFT_VAL = 16743045; //temp
-const int RIGHT_VAL = 16724175; //temp
+const int UP_VAL = -26521;
+const int DOWN_VAL = 14535;
+const int LEFT_VAL = 12495;
+const int RIGHT_VAL = 31365;
 int remote_val = 0;
 
 
@@ -30,68 +34,71 @@ void setup() {
   pinMode(enable, OUTPUT);
   pinMode(motor_dir_pin, OUTPUT);
   pinMode(motor_step_pin, OUTPUT);
+  pinMode(relay_pin1, OUTPUT);
+  pinMode(relay_pin2, OUTPUT);
+  
   irrecv.enableIRIn();
+  digitalWrite(motor_dir_pin, HIGH);
   
   Serial.begin(9600);
 }
 
 
-// loop every 500 ms
-// delay incremented for smoother IR operation
+// loop
 void loop() {
 
   // set actuator motor speed
   analogWrite(enable, 500);
 
-  // 1 if button pressed, 0 otherwise
-  if (irrecv.decode(&results)) {
+  // turn actuator off
+  digitalWrite(motor_in3, LOW);
+  digitalWrite(motor_in4, LOW);
 
-    // using ints has lot smaller values, use instead??
-    Serial.println(results.value);
-    print_remote_val(results.value);
-    irrecv.resume();
+  // without any button input, relays are 0 (brakes locked)
+  digitalWrite(relay_pin1, LOW);
+  digitalWrite(relay_pin2, LOW);
+  
+  // wait until any button is pressed
+  while(!(irrecv.decode(&results)));
+  irrecv.resume();
+  
+  print_remote_val(results.value);
 
-    // update remote variable only if button is not
-    // being pressed down
-    if (results.value != U_MAX_INT) {
-      remote_val = results.value;
-    }
-    
-    // based on remote_val set motor direction
-    switch (remote_val) {
-      case UP_VAL:
-        digitalWrite(motor_in3, HIGH);
-        digitalWrite(motor_in4, LOW);
-        delay(500);
-        break;
-      case DOWN_VAL:
+  // if button being held, don't update remote_val
+  if (int(results.value) != -1) {
+    remote_val = results.value;
+  }
+
+  // change rotational motor direction based on button
+  if (remote_val == LEFT_VAL) {
+    digitalWrite(motor_dir_pin, HIGH);
+  } else if (remote_val == RIGHT_VAL) {
+    digitalWrite(motor_dir_pin, LOW);
+  }
+
+  // choose proper motor action based on remote value
+  if (remote_val == LEFT_VAL || remote_val == RIGHT_VAL) {
+
+      // unlock brakes
+      digitalWrite(relay_pin1, HIGH);
+      digitalWrite(relay_pin2, HIGH);
+
+      // complete N steps
+      for(int i = 0; i < steps_per_rev; i++) {
+        digitalWrite(motor_step_pin, HIGH);
+        delayMicroseconds(10); 
+        digitalWrite(motor_step_pin, LOW); 
+        delayMicroseconds(10);
+      }
+    } else if (remote_val == UP_VAL) {
         digitalWrite(motor_in3, LOW);
         digitalWrite(motor_in4, HIGH);
         delay(500);
-        break;
-      case LEFT_VAL:
-        digitalWrite(motor_dir_pin, HIGH);
-        for(int x = 0; x < steps_per_rev; x++) {
-          digitalWrite(motor_step_pin, HIGH);
-          delayMicroseconds(10); 
-          digitalWrite(motor_step_pin, LOW); 
-          delayMicroseconds(10);
-        }
-      case RIGHT_VAL:
-        digitalWrite(motor_dir_pin, LOW);
-        for(int x = 0; x < steps_per_rev; x++) {
-          digitalWrite(motor_step_pin, HIGH);
-          delayMicroseconds(10); 
-          digitalWrite(motor_step_pin, LOW); 
-          delayMicroseconds(10);
-        }
-    }
-    
-  } else {
-    // no remote value so all motors off
-    digitalWrite(motor_in3, LOW);
-    digitalWrite(motor_in4, LOW);
-  }
+    } else if (remote_val == DOWN_VAL) {
+        digitalWrite(motor_in3, HIGH);
+        digitalWrite(motor_in4, LOW);
+        delay(500);
+    } 
 
 }
 
